da15e78e2246d33129f89e9eae1248a84b8b9f83
Anton Zhadan <86596115+anton-zhadan@users.noreply.github.com>
2021-10-11 15:15:02 +0300

    V1

---
+++ scripts.js
@@ -0,0 +1,38 @@
+points_all_answers = 0;
+
+// amount = prompt (`What's 2+2?`);
+// if (amount === '4') {
+//     alert ('Well done!');
+//     console.log (points_all_answers = 10);
+// } else {
+//     alert ('error');
+//     console.log (points_all_answers = 0);
+// }
+
+//sun = confirm (`Солнце встает на востоке?`);
+
+// if (sun) {
+//     console.log (points_all_answers = 10);
+// } 
+
+incorrect_question = confirm (`Сколько будет 5 / 0? Spoiler, press OK, i will give you 10 points=))`);
+
+if (incorrect_question) {
+    alert ('We will agree!'); 
+    console.log (sum = points_all_answers = 10);
+} else {
+    alert ('No, thats not how things are done!');
+    console.log (points_all_answers = 0);
+}
+
+color_sky = prompt (`What color is the sky?`).trim().toLowerCase();
+
+if (color_sky == 'blue') {
+    alert ('Good!');
+    console.log (sum = points_all_answers = 10); 
+} else {
+    alert ('Нет, пишите на английском!');
+    console.log (points_all_answers = 0); 
+} 
+
+alert (sum = points_all_answers);
