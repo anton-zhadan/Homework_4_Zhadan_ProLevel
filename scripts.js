f642d03e2b1bb408c9329123348339dbacc723b7
Anton Zhadan <86596115+anton-zhadan@users.noreply.github.com>
2021-10-13 13:19:56 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,29 +1,30 @@
 points_all_answers = 0;
 
-// amount = prompt (`What's 2+2?`);
-// if (amount === '4') {
-//     alert ('Well done!');
-//     console.log (points_all_answers += 10);
-// } else {
-//     alert ('error');
-// }
+amount = prompt (`What's 2+2?`);
+if (amount === '4') {
+    alert ('Well done!');
+    console.log (points_all_answers += 10);
+} else {
+    alert ('Error, enter correct value');
+}
 
-// sun = confirm (`The sun rises in the east?`);
+sun = confirm (`The sun rises in the east?`);
 
-// if (sun) {
-//     console.log (points_all_answers += 10);
-// } 
+if (sun) {
+    console.log (points_all_answers += 10);
+} 
 
-// incorrect_question = confirm (`How much 5 / 0? Spoiler, press OK, i will give you 10 points=))`);
+incorrect_question = confirm (`How much 5 / 0? Spoiler, press OK, i will give you 10 points=))`);
 
-// if (incorrect_question) {
-//     alert ('We will agree!'); 
-//     console.log (points_all_answers += 10);
-// } else {
-//     alert ('No, thats not how things are done!');
-// }
+if (incorrect_question) {
+    alert ('We will agree!'); 
+    console.log (points_all_answers += 10);
+} else {
+    alert ('No, thats not how things are done!');
+}
 
-color_sky = prompt (`What color is the sky?`).trim().toLowerCase();
+color_sky = prompt (`What color is the sky?`);//.trim().toLowerCase();
+color_sky = color_sky ? color_sky.trim().toLowerCase() : `;`
 
 switch (color_sky) {
     case 'blue':
@@ -32,23 +33,21 @@ switch (color_sky) {
     case 'blau': 
         console.log (points_all_answers += 10);
         break;
-    case '':
-    case null:
-        break;
     default:
-        alert ("Error, enter right value");
+        alert ("Error, enter correct value");
 }
 
-// philosophical_question = prompt (`What is the correct answer to the main question of life, the universe and all that?`).trim().toLowerCase();
+philosophical_question = prompt (`What is the correct answer to the main question of life, the universe and all that?`);
+philosophical_question = philosophical_question ? philosophical_question.trim().toLowerCase() : ``;
 
-// switch (philosophical_question) {
-//     case '42':
-//         alert ("Right, Deep Thought is proud of you!");
-//         console.log (points_all_answers += 10);
-//         break;
-//     default:
-//         //alert ("Don't right, ask Deep Thought!");
-//         break;
-// }
+switch (philosophical_question) {
+    case '42':
+        alert ("Right, Deep Thought is proud of you!");
+        console.log (points_all_answers += 10);
+        break;
+    default:
+        alert ("Don't right, ask Deep Thought!");
+        break;
+}
 
 alert (`Your result ${points_all_answers} points!`);
